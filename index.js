84f990bc929b7d1355bc7731e2f4e08e9177b745
Pablo Souza <pablo.souza@snet.net.br>
2019-02-12 16:28:44 -0200

    ajustando para mocha fechar o servidor

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('./server/config/express');
 
 // eslint-disable-next-line no-console
-app.listen(process.env.PORT, async () => console.log(`Server running at port ${process.env.PORT}`));
+if (!module.parent) {
+  app.listen(process.env.PORT, async () => console.log(`Server running at port ${process.env.PORT}`));
+}
 
 module.exports = app;
